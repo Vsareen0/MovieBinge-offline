f733489e3f286470aa5c5cbd418816489640db0f
vsareen0 <vsareen24@gmail.com>
2019-04-19 22:04:30 +0530

    Added Custom Controller [IndexController] for landing page

---
+++ MovieBingeOffline/src/main/resources/com/comaniacs/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package com.comaniacs.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping(value="/")
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping(value="/test")
+	public String test() {
+		return "test";
+	}
+}
